ac497b234b4a34ff4b3801e5165d510b4b21b0e4
SALAM <ktsalam17@gmail.com>
2025-01-20 13:00:03 +0400

    finall

--- src/components/StickyButton.jsx
+++ src/components/StickyButton.jsx
@@ -6,7 +6,7 @@ const StickyButton = () => {
   const handleDownload = () => {
     // Replace the URL with the path to your CV file
     const link = document.createElement("a");
-    link.href = "./../../public/abdusalamkt.pdf"; // Place your CV file in the public folder or use a hosted link
+    link.href = "/abdusalamkt.pdf"; // Correct the path to reference the file from the root
     link.download = "abdusalamkt.pdf";
     link.click();
   };
